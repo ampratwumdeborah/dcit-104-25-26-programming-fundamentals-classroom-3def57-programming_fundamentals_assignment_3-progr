5a4983270b16925bfa0a8a651e36a61895b661b0
Patrick Acheampong <kvngpatrickowusu@gmail.com>
2026-08-06 22:13:13 +0000

    Assignment 1

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -39,7 +39,34 @@
 
 //
 // =============================================================================
-// YOUR CODE BELOW — remove the // symbols from the scaffold and fill it in
+const readlineSync = require("readline-sync");
+
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function main() {
+    let number = readlineSync.questionInt("Enter a number: ");
+
+    if (isPrime(number)) {
+        console.log(number + " is a prime number.");
+    } else {
+        console.log(number + " is NOT a prime number.");
+    }
+}
+
+main();
+
 // =============================================================================
 
 
